f7f06783058298d4b90b82be129e142e4f9d4f76
Matt Reuter <mgr522@gmail.com>
2014-04-14 19:58:04 -0500

    Removed separate 1D/2D simulators; tweaked file names

--- Makefile
+++ Makefile
@@ -9,17 +9,13 @@ GSL_INCLUDE=$(GSL_DIR)/include
 INCLUDE=-I$(GSL_INCLUDE)
 LIBS=-L$(GSL_LIB) -lgsl -lgslcblas -lm
 
-all: simulator binner simulator-v-2d binner-v-2d
+all: simulator binner binner-v-2d
 #all: simulator binner fitter simulator-v-2d binner-v-2d
 	#cp simulator binner fitter simulator-v-2d binner-v-2d ../bin
 
-simulator: main-simulator.cc
-	$(CPP) $(CXXFLAGS) -o simulator main-simulator.cc \
-		$(INCLUDE) $(LIBS)
-
-simulator-v-2d: main-simulator-v-2d.cc aux_simulator string_tools.o
+simulator: main-simulator.cc aux_simulator string_tools.o
 	cd aux_simulator; make
-	$(CPP) $(CXXFLAGS) -o simulator-v-2d main-simulator-v-2d.cc string_tools.o \
+	$(CPP) $(CXXFLAGS) -o simulator main-simulator.cc string_tools.o \
 		aux_simulator/libaux_simulator.a $(INCLUDE) $(LIBS)
 
 binner: main-binner.cc
@@ -36,7 +32,7 @@ string_tools.o: string_tools.h string_tools.cc
 
 clean:
 	cd aux_simulator; make clean
-	rm -f *.o simulator binner fitter simulator-v-2d binner-v-2d
+	rm -f *.o simulator binner fitter binner-v-2d
 
 distclean:
 	rm -f *.o simulator binner fitter simulator-v-2d binner-v-2d ../bin/simulator ../bin/binner ../bin/fitter ../bin/simulator-v-2d ../bin/binner-v-2d
--- main-simulator.cc
+++ main-simulator.cc
@@ -1,67 +1,59 @@
-/*
-This work is licensed under the Creative Commons Attribution 3.0 United States
-License. To view a copy of this license, visit
-http://creativecommons.org/licenses/by/3.0/us/ or send a letter to Creative
-Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
-
-Copyright (C) 2013 Oak Ridge National Laboratory
-*/
 /**
- * \file main-simulator.cc
+ * \file main-simulator-v-2d.cc
  * \brief Main function for simulating conductance data using Landauer theory.
  *
  * Statistical parameters (for example, the average level energy) are provided
  * on the command-line and used to simulate conductance data. This data can
  * subsequently be binned into a histogram to test the fitting procedures.
  *
- * Two models are presently implemented:
- *    - Symmetric couplings (same coupling used for the left and right leads).
- *    - Asymmetric couplings (different couplings for the two leads).
+ * This code produces voltage-dependent conductance histograms. Two types are
+ * presently implemented:
+ *    - Static conductance (I/V)
+ *    - Differential conductance (dI/dV)
+ * Symmetric coupling (same coupling to the left and right leads) is assumed,
+ * unless otherwise specified.
  *
- * There are seven or eight required command-line arguments, depending on the
- * model:
- *    -# The model to use (`s' for symmetric, `a' for asymmetric).
- *    -# The number of conductance data points to simulate.
- *    -# The Fermi level of the system (eV)
- *    -# The standard deviation in site level energy (eV)
- *    -# The average site level energy (eV)
- *    -# The standard deviation in electrode-channel coupling (eV)
- *    -# The average coupling to one electrode (asymmetric model) or to both
- *       electrodes (symmetric model), in eV.
- *    -# The average coupling to the other electrode (eV). Only required if the
- *       asymmetric model is used.
- *
- * \author Patrick D.\ Williams and Matthew G.\ Reuter
- * \date July 2012, May 2013
+ * \author Matthew G.\ Reuter
+ * \date April 2014
  */
 
+#include <memory>
 #include <cstdio>
-#include <cstdlib>
-#include <gsl/gsl_rng.h>
-#include <gsl/gsl_randist.h>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <functional>
+#include <map>
+#include "string_tools.h"
+#include "aux_simulator/rng.h"
+#include "aux_simulator/symmetric_voltage_independent.h"
+#include "aux_simulator/asymmetric_voltage_independent.h"
+#include "aux_simulator/symmetric_voltage_one_site.h"
+
+using namespace std;
 
 /**
- * \brief Samples from a normal distribution with the given mean and standard
- *        deviation.
+ * \brief Enum for the types of conductance calculations we have.
+ */
+enum class CalculationType {
+	Static, // Voltage-Dependent Static Conductance
+	Differential, // Voltage-Dependend Differential Conductance
+	ZeroBias // Zero-Bias Differential Conductance
+};
+
+/**
+ * \brief Takes the list of distributions from the input deck and constructs
+ *    a model of the specified type.
  *
- * \param[in] mean The distribution's average.
- * \param[in] stdev The distribution's standard deviation.
- * \param[in] r The internal workspace for GSL's random number generation.
- * \return The random number.
+ * \throw invalid_argument if any required distributions are not specified.
+ *
+ * \param[in] str The name of the type, assumed to be lowercase.
+ * \param[in] parameters Map of distribution names (lowercase) to the
+ *    distributions.
+ * \return A shared pointer to the model object.
  */
-double normal_random_variable(double mean, double stdev, gsl_rng *r);
-
-/**
- * \brief Evalutates the transmission (Landauer theory) using the specified
- *        physical parameters.
- * 
- * \param[in] gamma1 One channel-lead coupling.
- * \param[in] gamma2 The other channel-lead coupling.
- * \param[in] epsilon The channel's level energy.
- * \param[in] E The Fermi level.
- * \return The transmission.
- */
-double transmission(double gamma1, double gamma2, double epsilon, double E);
+shared_ptr<ConductanceModel> set_distributions(const string str,
+	const map<string, shared_ptr<RandomDistribution>> &parameters);
 
 /**
  * \brief Main function for simulating a histogram.
@@ -74,118 +66,355 @@ double transmission(double gamma1, double gamma2, double epsilon, double E);
  * \return Exit status; 0 for normal.
  */
 int main(int argc, char **argv) {
-	const gsl_rng_type *T;
-	gsl_rng *r;
-     
+
+	// initialize the GSL random number generator
+	gsl_rng_env_setup();
+	shared_ptr<gsl_rng> r(gsl_rng_alloc(gsl_rng_default), &gsl_rng_free);
+	gsl_rng_set(r.get(), 0xFEEDFACE);
+
+	CalculationType type;
 	int i, n;
-	double gammaL, gammaR, epsilon, trans;
-	double gamma1, gamma2;
-	double dgamma;
-	double epsilon0;
-	double depsilon;
-	double E;
-	char model;
+	double EF;
+	map<string, shared_ptr<RandomDistribution>> parameters;
+	shared_ptr<ConductanceModel> model;
+	shared_ptr<RandomDistribution> dist_V, dist_eta;
+	function<void(void)> conductance_function;
 
-	if(argc != 8 && argc != 9) {
-		fprintf(stderr, "Usage error: ./simulator model n EF deps eps0 dgamma " \
-			"gamma1 gamma2 n\n" \
-			"   model is the model to use ('s' for symmetric, 'a' for " \
-				"asymmetric)\n" \
-			"   n is the number of trials\n" \
-			"   EF is the Fermi level (eV)\n" \
-			"   deps is the standard deviation in site level energy (eV)\n" \
-			"   eps0 is the average site level energy (eV)\n" \
-			"   dgamma is the standard deviation in the coupling (eV)\n" \
-			"   gamma1 is the average coupling for one electrode (eV)\n" \
-			"   gamma2 is the average coupling for the other electrode (eV)\n" \
-			"\n   NOTE: gamma2 is ignored (and unnecessary) if model == 's'.\n");
+	string line, modeltype, name;
+	vector<string> tokens;
+
+	// setup the simulation -- read in parameters from stdin
+	// Line 1: One token specifying the model to use
+	try {
+		line = getline(stdin);
+	}
+	catch(const runtime_error &e) {
+		fprintf(stderr, "Error: %s.\n", e.what());
 		return 0;
 	}
 
-	model = argv[1][0];
-	if(model != 'a' && model != 's') {
-		fprintf(stderr, "Error: Unknown model. Must be 's' or 'a'.\n");
+	tokenize(line, tokens);
+	if(tokens.size() < 1) {
+		fprintf(stderr, "Error: model name expected in line 1.\n");
 		return 0;
 	}
 
-	n = atoi(argv[2]);
-	E = atof(argv[3]);
-	depsilon = atof(argv[4]);
-	epsilon0 = atof(argv[5]);
-	dgamma = atof(argv[6]);
-	gamma1 = atof(argv[7]);
-	if(model == 'a') {
-		if(argc == 9)
-			gamma2 = atof(argv[8]);
-		else {
-			fprintf(stderr, "Error: Not enough parameters for the asymmetric " \
-				"model.\n       Run ./simulator for the correct usage.\n");
+	// make a link to the correct model creator (need to continue reading data,
+	// store for later)
+	modeltype = tokens[0];
+	make_lower(modeltype);
+
+	// Line 2: Type of conductance to calculate
+	try {
+		line = getline(stdin);
+	}
+	catch(const runtime_error &e) {
+		fprintf(stderr, "Error: %s.\n", e.what());
+		return 0;
+	}
+	tokenize(line, tokens);
+	if(tokens.size() < 1) {
+		fprintf(stderr, "Error: conductance type expected in line 2.\n");
+		return 0;
+	}
+	make_lower(tokens[0]);
+	if(tokens[0] == "static")
+		type = CalculationType::Static;
+	else if(tokens[0] == "differential")
+		type = CalculationType::Differential;
+	else if(tokens[0] == "zerobias")
+		type = CalculationType::ZeroBias;
+	else {
+		fprintf(stderr, "Error: Unrecognized conductance type in line 2.\n   " \
+			"It must be \"Static\", \"Differential\", or \"ZeroBias\".\n");
+		return 0;
+	}
+
+	// Line 3: number of trials
+	try {
+		line = getline(stdin);
+	}
+	catch(const runtime_error &e) {
+		fprintf(stderr, "Error: %s.\n", e.what());
+		return 0;
+	}
+	tokenize(line, tokens);
+	if(tokens.size() < 1) {
+		fprintf(stderr, "Error: number of trials expected in line 3.\n");
+		return 0;
+	}
+	try {
+		n = stoi(tokens[0]);
+	}
+	catch(const invalid_argument &e) {
+		fprintf(stderr, "Error: unrecognizable number '%s'.\n",
+			tokens[0].c_str());
+		return 0;
+	}
+
+	// Line 4: Fermi level
+	try {
+		line = getline(stdin);
+	}
+	catch(const runtime_error &e) {
+		fprintf(stderr, "Error: %s.\n", e.what());
+		return 0;
+	}
+	tokenize(line, tokens);
+	if(tokens.size() < 1) {
+		fprintf(stderr, "Error Fermi energy expected in line 4.\n");
+		return 0;
+	}
+	try {
+		EF = stod(tokens[0]);
+	}
+	catch(const invalid_argument &e) {
+		fprintf(stderr, "Error: unable to parse %s to the Fermi energy.\n",
+			tokens[0].c_str());
+		return 0;
+	}
+
+	// all subsequent lines specify random number distributions
+	// EOF is flagged by a runtime_error in the getline function
+	try {
+		while(true) {
+			line = getline(stdin);
+
+			tokenize(line, tokens);
+			if(tokens.size() > 0) {
+				name = tokens[0];
+				make_lower(name);
+
+				// need to remove the front entry for the distribution generator
+				tokens.erase(tokens.begin());
+
+				// create a random distribution from the contents of the line
+				try {
+					parameters[name] = distribution_from_tokens(tokens);
+				}
+				catch(const invalid_argument &e) {
+					fprintf(stderr, "Error: unable to form a random number " \
+						"distribution from:\n   %s\n%s\n", line.c_str(), e.what());
+				}
+			}
+		}
+	}
+	catch(const runtime_error &e) {
+	}
+
+	// set the distributions required by the model
+	try {
+		model = set_distributions(modeltype, parameters);
+	}
+	catch(const invalid_argument &e) {
+		fprintf(stderr, "Error initializing the model: %s\n", e.what());
+		return 0;
+	}
+
+	// set up the run function for each type...
+	// this may require other distributions to be set
+	if(type == CalculationType::Static ||
+		type == CalculationType::Differential) {
+
+		// make sure there are distributions for V and eta
+		try {
+			dist_eta = parameters.at("eta");
+		}
+		catch(const out_of_range &e) {
+			fprintf(stderr, "Error: a distribution for \"eta\" must be " \
+				"specified.\n");
 			return 0;
 		}
-	}
+		try {
+			dist_V = parameters.at("v");
+		}
+		catch(const out_of_range &e) {
+			fprintf(stderr, "Error: a distribution for \"V\" must be specified." \
+				"\n");
+			return 0;
+		}
 
-	if(depsilon <= 0.0 || dgamma <= 0.0) {
-		fprintf(stderr, "Error: standard deviations must be positive.\n");
+		// set the conductance function
+		if(type == CalculationType::Static) {
+			conductance_function = [=] () {
+				double V = dist_V->sample(r);
+				double GV = model->static_conductance(r, EF, dist_eta->sample(r),
+					V);
+				printf("%.6f %.6f\n", V, GV);
+			};
+		}
+		else if(type == CalculationType::Differential) {
+			conductance_function = [=] () {
+				double V = dist_V->sample(r);
+				double GV = model->diff_conductance(r, EF, dist_eta->sample(r), V);
+				printf("%.6f %.6f\n", V, GV);
+			};
+		}
+	}
+	else if(type == CalculationType::ZeroBias) {
+		// no extra distributions required... proceed to the calculation
+		conductance_function = [=] () {
+			printf("%.6f\n", model->zero_bias_conductance(r, EF));
+		};
+	}
+	else {
+		// should never be here
+		fprintf(stderr, "An unknown error has occured.\n");
 		return 0;
 	}
 
-	if(gamma1 <= 0.0) {
-		fprintf(stderr, "Error: gamma1 must be positive.\n");
-		return 0;
-	}
-
-	if(model == 'a' && gamma2 <= 0.0) {
-		fprintf(stderr, "Error: gamma2 must be positive.\n");
-		return 0;
-	}
-
-	if(n <= 0) {
-		fprintf(stderr, "Error: There must be at least one trial.\n");
-		return 0;
-	}
-
-	if(gamma1 / dgamma < 4.0 || (model == 'a' && gamma2 / dgamma < 4.0)) {
-		fprintf(stderr, "Warning: The models assume gamma1(2) / dgamma >> 0; " \
-			"bigger than 4, in practice.\n");
-	}
-
-	// Setup the GSL random number generator
-	gsl_rng_env_setup();
-	T = gsl_rng_default;
-	r = gsl_rng_alloc(T);
-
-	// seed the generator
-	gsl_rng_set(r, 0xFEEDFACE);
-
-	// Get the requested number of transmission values
+	// Get the requested number of voltage-conductance data points
 	for (i = 0; i < n; ++i) {
-		gammaL = normal_random_variable(gamma1, dgamma, r);
-		epsilon = normal_random_variable(epsilon0, depsilon, r);
-
-		if(model == 's') {
-			trans = transmission(gammaL, gammaL, epsilon, E);
-		}
-		else if(model == 'a') {
-			gammaR = normal_random_variable(gamma2, dgamma, r);
-			trans = transmission(gammaL, gammaR, epsilon, E);
-		}
-		else {
-			fprintf(stderr, "An impossible error has occurred.\n");
-			trans = 0.0;
-		}
-
-		printf("%.6f\n", trans);
+		conductance_function();
 	}
 
-	gsl_rng_free(r);
 	return 0;
 }
 
-double normal_random_variable(double mean, double stdev, gsl_rng *r) {
-	return gsl_ran_gaussian(r, stdev) + mean;
+shared_ptr<ConductanceModel> set_distributions(const string str,
+	const map<string, shared_ptr<RandomDistribution>> &parameters) {
+
+	// create the actual model and process the random number distributions
+	if(str == "symmetricvoltageindependentmodel") {
+		shared_ptr<RandomDistribution> dist_gamma, dist_eps;
+
+		// populate the gamma and epsilon distributions
+		try {
+			dist_gamma = parameters.at("gamma");
+		}
+		catch(const out_of_range &e) {
+			throw invalid_argument("A distribution for \"gamma\" must be " \
+				"specified.");
+		}
+
+		try {
+			dist_eps = parameters.at("epsilon");
+		}
+		catch(const out_of_range &e) {
+			throw invalid_argument("A distribution for \"epsilon\" must be " \
+				"specified.");
+		}
+
+		return make_shared<SymmetricVoltageIndependentModel>(dist_eps,
+			dist_gamma);
+	}
+	else if(str == "symmetricvoltageonesitemodel") {
+		shared_ptr<RandomDistribution> dist_gamma, dist_eps;
+
+		// populate the gamma and epsilon distributions
+		try {
+			dist_gamma = parameters.at("gamma");
+		}
+		catch(const out_of_range &e) {
+			throw invalid_argument("A distribution for \"gamma\" must be " \
+				"specified.");
+		}
+
+		try {
+			dist_eps = parameters.at("epsilon");
+		}
+		catch(const out_of_range &e) {
+			throw invalid_argument("A distribution for \"epsilon\" must be " \
+				"specified.");
+		}
+
+		return make_shared<SymmetricVoltageOneSiteModel>(dist_eps, dist_gamma);
+	}
+	else if(str == "asymmetricvoltageindependentmodel") {
+		shared_ptr<RandomDistribution> dist_gammaL, dist_gammaR, dist_eps;
+
+		// populate the gamma and epsilon distributions
+		try {
+			dist_gammaL = parameters.at("gammal");
+		}
+		catch(const out_of_range &e) {
+			throw invalid_argument("A distribution for \"gammaL\" must be " \
+				"specified.");
+		}
+
+		try {
+			dist_gammaR = parameters.at("gammar");
+		}
+		catch(const out_of_range &e) {
+			throw invalid_argument("A distribution for \"gammaR\" must be " \
+				"specified.");
+		}
+
+		try {
+			dist_eps = parameters.at("epsilon");
+		}
+		catch(const out_of_range &e) {
+			throw invalid_argument("A distribution for \"epsilon\" must be " \
+				"specified.");
+		}
+
+		return make_shared<AsymmetricVoltageIndependentModel>(dist_eps,
+			dist_gammaL, dist_gammaR);
+	}
+	else
+		throw invalid_argument("Unrecognized model. Options are:\n" \
+			"   SymmetricVoltageIndependentModel - " \
+				"Symmetric-Coupling, Voltage-Independent Transmission\n" \
+			"   SymmetricVoltageOneSiteModel - " \
+				"Symmetric-Coupling, Voltage-Dependent One-Site Model\n" \
+			"   AsymmetricVoltageIndependentModel - " \
+				"Asymmetric-Coupling, Voltage-Independent Transmission\n");
+
+	// should never be here
+	throw invalid_argument("Shouldn't be here.");
+	return nullptr;
 }
 
-double transmission(double gamma1, double gamma2, double epsilon, double E) {
-	return 4.0*gamma1*gamma2 / 
-		(4.0*(E-epsilon)*(E-epsilon) + (gamma1 + gamma2)*(gamma1 + gamma2));
+#if 0
+// Double-site, voltage-dependent model
+static double transmission_d(double V, double gamma, double epsilon,
+	double beta, double E) {
+
+	double temp = 4.*(E-epsilon)*(E-epsilon) - 4.*beta*beta - gamma*gamma - V*V;
+
+	return 16.*gamma*gamma*beta*beta / (temp*temp +
+		16.*gamma*gamma*(E-epsilon)*(E-epsilon));
 }
+
+static double dtdvint_d(double V, double gamma, double beta, double z) {
+	const double bv = 4.*beta*beta + V*V;
+	const double bvg = bv + gamma*gamma;
+	const std::complex<double> arctan = atan(2.*z /
+		std::complex<double>(gamma, -sqrt(bv)));
+
+	return 8.*V*gamma*gamma*beta*beta*z*(4.*z*z + gamma*gamma - 3.*bv) /
+		(bv*bvg*(16.*z*z*z*z + 8.*(gamma*gamma - bv)*z*z + bvg*bvg))
+
+		- 8.*V*gamma*beta*beta / (bvg*bvg) * std::real(arctan)
+
+		- 4.*V*gamma*gamma*beta*beta*(3.*bv + gamma*gamma) /
+			(bvg*bvg*bv*sqrt(bv)) * std::imag(arctan);
+}
+
+double diff_conductance_d(double V, double gamma, double epsilon, double eta,
+	double EF) {
+
+	const double beta = -3.0;
+
+	return eta*transmission_d(V, gamma, epsilon, beta, EF + eta*V) +
+		(1.-eta)*transmission_d(V, gamma, epsilon, beta, EF + (eta-1.)*V) +
+		dtdvint_d(V, gamma, beta, EF - epsilon + eta*V) -
+		dtdvint_d(V, gamma, beta, EF - epsilon + (eta-1.)*V);
+}
+
+static double tint_d(double V, double gamma, double beta, double z) {
+	const std::complex<double> arg(gamma, -sqrt(4.*beta*beta + V*V));
+
+	return 4.*beta*beta*gamma/(V * sqrt(4.*beta*beta+V*V)) *
+		std::imag(atan(2.*z/arg)/arg);
+}
+
+double static_conductance_d(double V, double gamma, double epsilon, double eta,
+	double EF) {
+
+	const double beta = -3.0;
+
+	return tint_d(V, gamma, beta, EF - epsilon + eta*V)
+		- tint_d(V, gamma, beta, EF - epsilon + (eta - 1.)*V);
+}
+#endif
