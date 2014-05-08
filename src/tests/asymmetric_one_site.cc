308d5314bf3cb4170eed152b3a58437a2d2c4add
Matt Reuter <mgr522@gmail.com>
2014-05-08 13:15:33 -0500

    Tests for the asymmetric-coupling, one-site model

---
+++ src/tests/asymmetric_one_site.cc
@@ -0,0 +1,56 @@
+/**
+ * \file asymmetric_one_site.cc
+ * \brief Test suite for the asymmetric-coupling, single-site tight-binding
+ *     model.
+ *
+ * \author Matthew G.\ Reuter
+ * \date May 2014
+ */
+
+#include <cstdio>
+#include <assert.h>
+#include <cmath>
+#include "../aux_simulator/asymmetric_one_site.h"
+
+using namespace std;
+
+/**
+ * \brief Main function for testing the symmetric-coupling, one-site model.
+ *
+ * \param[in] argc The number of command-line arguments.
+ * \param[in] argv The command-line arguments.
+ * \return Exit status: 0 if the code passes the test, non-zero otherwise.
+ */
+int main(int argc, char **argv) {
+	const double thresh = 1.0e-6;
+
+	// check known values for several parameter sets
+	assert(abs(0.0475907
+		- AsymmetricOneSiteModel::transmission(0., -4., 0.8, 1.0)) < thresh);
+	assert(abs(0.0482617
+		- AsymmetricOneSiteModel::static_conductance(0., 1., 0.5, -4., 0.8, 1.0))
+		< thresh);
+	assert(abs(0.0496212
+		- AsymmetricOneSiteModel::diff_conductance(0., 1., 0.5, -4., 0.8, 1.0))
+		< thresh);
+
+	assert(abs(0.000799281
+		- AsymmetricOneSiteModel::transmission(1., -9., 0.4, 0.2)) < thresh);
+	assert(abs(0.000819131
+		- AsymmetricOneSiteModel::static_conductance(1., -0.4, 0.8, -9., 0.4, 0.2))
+		< thresh);
+	assert(abs(0.000839689
+		- AsymmetricOneSiteModel::diff_conductance(1., -0.4, 0.8, -9., 0.4, 0.2))
+		< thresh);
+
+	assert(abs(0.00330201
+		- AsymmetricOneSiteModel::transmission(3., -17., 0.67, 1.98)) < thresh);
+	assert(abs(0.00347858
+		- AsymmetricOneSiteModel::static_conductance(3., 1.4, 0.14, -17., 0.67, 1.98))
+		< thresh);
+	assert(abs(0.00366672
+		- AsymmetricOneSiteModel::diff_conductance(3., 1.4, 0.14, -17., 0.67, 1.98))
+		< thresh);
+
+	return 0;
+}
