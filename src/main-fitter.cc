e8ffccbe972108968783dc8f349b22f1deeb9cea
Matt Reuter <mgr522@gmail.com>
2014-05-15 15:33:52 -0500

    Some work on the fitter program. It compiles, but needs I/O functionality.

--- src/main-fitter.cc
+++ src/main-fitter.cc
@@ -3,47 +3,39 @@
  * \brief Main function for fitting histogram data to the desired functional
  *        form.
  *
- * This program fits conductance data (using one of the models listed below)
+ * This program fits conductance data (using one of the implemented models)
  * and outputs the best-fit parameters. Since all of these functional forms
  * are non-linear, multiple initial guesses are used, and the overall best fit
  * is output.
  *
- * Available functional forms are:
- *    - `n' The non-resonant tunneling (symmetric channel-lead coupling) model.
- *    - `r' The resonant tunneling (symmetric channel-lead coupling) model.
- *    - `a' The resonant tunneling (asymmetric channel-lead coupling) model.
- *
- * There are two required command-line arguments, and another optional one.
- *    -# The number of bins in the histogram (number of data points to read
- *       from standard input).
- *    -# The model to use (see above for character flags).
- *    -# [Optional] Produce iteration-to-iteration output for the nonlinear
- *       fitting process. This can produce a lot of output.
- *
  * \author Matthew G.\ Reuter
- * \date July 2012, May 2013
+ * \date May 2014
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 #include <gsl/gsl_blas.h>
+
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <list>
+#include <utility>
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_multifit_nlin.h>
-#include <queue>
-
-#include "aux_fitter/models.h"
+#include "aux_fitter/fit_model_1d.h"
 
 using namespace std;
 
+#if 0
 /**
  * \brief Prints the status of the nonlinear fit after each iteration.
  *
  * \param[in] iter The present iteration.
- * \param[in] solver The GSL solver.
+ * \param[in] s The GSL solver.
  * \param[in] model The model being used.
  */
-void iteration_print(size_t iter, gsl_multifit_fdfsolver *s, char model);
+void iteration_print(size_t iter, shared_ptr<gsl_multifit_fdfsolver> s,
+	char model);
+#endif
 
 /**
  * \brief Main function.
@@ -56,77 +48,46 @@ void iteration_print(size_t iter, gsl_multifit_fdfsolver *s, char model);
  * \return Exit status; 0 for normal.
  */
 int main(int argc, char **argv) {
-	int status;
-	size_t i, iter, nbin;
-	size_t params, normparams;
-	double *g, *pdf;
-	gsl_multifit_fdfsolver *solver;
+	// non-linear fitting tools
+	shared_ptr<gsl_multifit_fdfsolver> solver;
 	gsl_multifit_function_fdf fdf;
-	st_data data;
-	gsl_vector *vec, *bestfit, *beststdev;
-	gsl_matrix *covar;
-	double chi, dof, rel;
-	char model;
-	queue<double> initvals;
+
+	// status of the fit
+	shared_ptr<gsl_vector> vec, bestfit, beststdev;
 	bool hasfit, iterprint;
 	double resid, bestresid;
-	double fits[3];
-	double norm;
+	int status;
 
-	// get the command-line arguments
-	if(argc != 3 && argc != 4) {
-		fprintf(stderr, "Usage: ./fitter nbin model [print]\n" \
-			"   nbin is the number of bins in the histogram.\n" \
-			"   model is the model to fit against.\n" \
-			"      - 'n' for non-resonant tunneling, symmetric coupling\n" \
-			"      - 'r' for resonant tunneling, symmetric coupling\n" \
-			"      - 'a' for resonant tunneling, asymmetric coupling\n" \
-			"   [print] is an optional argument that produces output after each" \
-			" iteration of the\n           non-linear fit routine. This can "\
-			"produce a LOT of output.\n" \
-			"NOTE: The data is expected through stdin.\n");
-		return 0;
-	}
+	// model & parameters
+	shared_ptr<FitModel<1>> model;
+	list<pair<array<double, 1>, double>> data;
+	list<vector<double>> initvals;
+	list<vector<double>>::const_iterator initval;
 
-	nbin = atoi(argv[1]);
-	if(nbin < 1) {
-		fprintf(stderr, "Error: Use at least one bin.\n");
-		return 0;
-	}
-
-	model = argv[2][0];
-	if(model != 'r' && model != 'n' && model != 'a') {
-		fprintf(stderr, "Error: Model must be 'r', 'n', or 'a' for the\n" \
-			"resonant (symmetric), non-resonant (symmetric), and resonant\n" \
-			"(asymmetric) models, respectively.\n");
-		return 0;
-	}
-
-	iterprint = (argc == 4);
+	// counters & auxiliary variables
+	size_t i, iter;
 
 	gsl_set_error_handler_off();
 
-	g = (double*)malloc(nbin*sizeof(double));
-	pdf = (double*)malloc(nbin*sizeof(double));
+	// read in the data points from the STDIN
+	size_t nbin = 39;
+	for(i = 0; i < nbin; ++i) {
+		double g, pdf;
+		scanf("%le %le", &g, &pdf);
 
-	// read in the data points from STDIN
-	for(i = 0; i < nbin; ++i)
-		scanf("%le %le", g + i, pdf + i);
+		data.emplace_back(pair<array<double, 1>, double>({{g}}, pdf));
+	}
 
-	// setup the data for GSL
-	data.n = nbin;
-	data.g = g;
-	data.pdf = pdf;
-	data.w = NULL;
-	fdf.n = nbin;
-	fdf.params = &data;
+	// set the model
+	model = get_fit_model("symmetricresonant", data);
 
-	// a note on the initial values:
-	// since each model uses a various number of parameters, the initvals
-	// queue just stores each parameter as an item.
-	// For example, on each trial in the 'r' model, only 1 item is read
-	// from initvals; but for each trial in 'n', 2 items are read.
+	// set up GSL details
+	fdf = model->gsl_handle();
+	vec.reset(gsl_vector_alloc(model->nfit), &gsl_vector_free);
+	bestfit.reset(gsl_vector_alloc(model->nfit), &gsl_vector_free);
+	beststdev.reset(gsl_vector_alloc(model->nfit), &gsl_vector_free);
 
+#if 0
 	// model-specific setup
 	if(model == 'n') {
 		params = 2; // number of parameters in the fit
@@ -139,26 +100,6 @@ int main(int argc, char **argv) {
 			initvals.push(cvals[i]);
 			initvals.push(dvals[j]);
 		}
-
-		// initialize GSL
-		fdf.f = &symmetric_coupling_nonresonance_f;
-		fdf.df = &symmetric_coupling_nonresonance_df;
-		fdf.fdf = &symmetric_coupling_nonresonance_fdf;
-	}
-	else if(model == 'r') {
-		params = 1; // number of parameters in the fit
-
-		// set up some initial value guesses
-		initvals.push(5.0);
-		initvals.push(10.0);
-		initvals.push(20.0);
-		initvals.push(35.0);
-		initvals.push(50.0);
-
-		// initialize GSL
-		fdf.f = &symmetric_coupling_resonance_f;
-		fdf.df = &symmetric_coupling_resonance_df;
-		fdf.fdf = &symmetric_coupling_resonance_fdf;
 	}
 	else if(model == 'a') {
 		params = 3; // number of parameters in the fit
@@ -174,47 +115,43 @@ int main(int argc, char **argv) {
 			initvals.push(cvals[k]);
 		}
 
-		// initialize GSL
-		fdf.f = &asymmetric_coupling_resonance_f;
-		fdf.df = &asymmetric_coupling_resonance_df;
-		fdf.fdf = &asymmetric_coupling_resonance_fdf;
-
 		// allocate numerical integration workspace
 		data.w = gsl_integration_workspace_alloc(2000);
 	}
 	normparams = params + 1; // number of parameters, including the norm
+#endif
 
-	vec = gsl_vector_alloc(normparams);
-	bestfit = gsl_vector_alloc(normparams);
-	beststdev = gsl_vector_alloc(normparams);
-	fdf.p = normparams;
-
-	solver = gsl_multifit_fdfsolver_alloc(gsl_multifit_fdfsolver_lmsder, nbin,
-		normparams);
+	solver.reset(
+		gsl_multifit_fdfsolver_alloc(gsl_multifit_fdfsolver_lmsder, nbin,
+			model->nfit),
+		&gsl_multifit_fdfsolver_free);
 
 	// we don't have a successful fit at the start
 	hasfit = false;
 
-	// go through all the initial values
-	do {
+	// perform fits with all the initial values
+	initvals = model->initial_guesses();
+	for(initval = initvals.cbegin(); initval != initvals.cend(); ++initval) {
 		// load the initial values
-		for(i = 0; i < params; ++i) {
-			gsl_vector_set(vec, i, initvals.front());
-			initvals.pop();
+		for(i = 0; i < model->nfit; ++i) {
+			gsl_vector_set(vec.get(), i, (*initval)[i]);
 		}
-		gsl_vector_set(vec, params, 1.0);
-		gsl_multifit_fdfsolver_set(solver, &fdf, vec);
+		gsl_multifit_fdfsolver_set(solver.get(), &fdf, vec.get());
 
 		// start iterating
 		iter = 0;
+#if 0
 		if(iterprint)
 			iteration_print(iter, solver, model);
+#endif
 
 		do {
 			++iter;
-			status = gsl_multifit_fdfsolver_iterate(solver);
+			status = gsl_multifit_fdfsolver_iterate(solver.get());
+#if 0
 			if(iterprint)
 				iteration_print(iter, solver, model);
+#endif
 
 			if(status)
 				break;
@@ -243,29 +180,20 @@ int main(int argc, char **argv) {
 		// do some final processing
 		resid = gsl_blas_dnrm2(solver->f);
 		if(!hasfit || resid < bestresid) {
-			covar = gsl_matrix_alloc(normparams, normparams);
-			gsl_multifit_covar(solver->J, 0.0, covar);
-			chi = gsl_blas_dnrm2(solver->f);
-			dof = nbin - normparams;
-			rel = GSL_MAX_DBL(1, chi / sqrt(dof));
-
-			// copy over the parameters and standard deviations
+			// copy over the parameters
 			bestresid = resid;
-			for(i = 0; i < normparams; ++i) {
-				gsl_vector_set(bestfit, i, gsl_vector_get(solver->x, i));
-				gsl_vector_set(beststdev, i,
-					rel*sqrt(gsl_matrix_get(covar, i, i)));
-			}
+			for(i = 0; i < model->nfit; ++i)
+				gsl_vector_set(bestfit.get(), i, gsl_vector_get(solver->x, i));
 
-			gsl_matrix_free(covar);
 			hasfit = true;
 		}
-	} while(!initvals.empty());
+	}
 
 	// did we get a fit?
 	if(!hasfit) {
 		fprintf(stderr, "Error fitting.\n");
 	}
+#if 0
 	else {
 		// do some post-processing (i.e., the fit might have let the gammas
 		// become negative, etc.)
@@ -320,17 +248,15 @@ int main(int argc, char **argv) {
 	// free all allocations
 	if(model == 'a')
 		gsl_integration_workspace_free(data.w);
-	gsl_multifit_fdfsolver_free(solver);
-	gsl_vector_free(beststdev);
-	gsl_vector_free(bestfit);
-	gsl_vector_free(vec);
-	free(pdf);
-	free(g);
+#endif
 
 	return 0;
 }
 
-void iteration_print(size_t iter, gsl_multifit_fdfsolver *s, char model) {
+#if 0
+void iteration_print(size_t iter, shared_ptr<gsl_multifit_fdfsolver> s,
+	char model) {
+
 	if(model == 'n') {
 		printf("iter %04u: c=%.4e d=%.4e resid=%.4e\n", (unsigned int)iter,
 			gsl_vector_get(s->x, 0), gsl_vector_get(s->x, 1),
@@ -346,3 +272,4 @@ void iteration_print(size_t iter, gsl_multifit_fdfsolver *s, char model) {
 			gsl_vector_get(s->x, 2), gsl_blas_dnrm2(s->f));
 	}
 }
+#endif
