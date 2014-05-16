267141d8245fe6891c758f6ee4e0e5b39f95c230
Matt Reuter <mgr522@gmail.com>
2014-05-16 11:47:38 -0500

    Documentation for the ymmetric, resonant-tunneling fit model.

--- src/aux_fitter/symmetric_resonant.h
+++ src/aux_fitter/symmetric_resonant.h
@@ -12,8 +12,29 @@
 #include "fit_model.h"
 
 /**
- * \brief Abstract class encapsulating models that data be fit to.
+ * \brief The fit model for resonant tunneling through a single site with
+ *    symmetric electrode/site couplings.
  *
+ * The line shape for resonant tunneling through a single site with symmetric
+ * coupling to the leads is \f[
+ * \hat{P}(g) = \frac{N}{\sqrt{g^3(1-g)}} \exp\left[ -\gamma^2 \frac{1-g}{2g} \right],
+ * \f] where \f$g\f$ is the conductance in atomic units. The fitting parameters
+ * are \f$\gamma\f$, the average channel coupling relative to the standard
+ * deviation in the level alignment, and \f$N\f$, the normalization constant.
+ *
+ * This form is based on a symmetric-coupling single-site model
+ * (SymmetricOneSiteModel) with normal distributions (NormalDistribution) on
+ * \f$\varepsilon\f$ and \f$\Gamma\f$.
+ *
+ * Since this functional form is singular as \f$g\to1\f$, the line shape spans
+ * several orders of magnitude, and possibly causing problems during the fit.
+ * Specifically, the fit essentially overweights the points close to \f$g=1\f$;
+ * their higher magnitudes leads to larger absolute residuals. To compensate,
+ * we scale the residuals by the magnitude of the observed value; this, in
+ * effect, provides a more equal weighting for all points and provides more
+ * accurate fits.
+ *
+ * This model is detailed in Reference \cite williams-5937.
  */
 class SymmetricResonantFitModel : public FitModel<1> {
 public:
@@ -59,6 +80,12 @@ public:
 	/**
 	 * \brief Calculates the Jacobian of the fit function for a given set of
 	 *    fitting parameters and a specific set of independent variables.
+	 *
+	 * For reference, the elements of the Jaocbian are given by
+	 * \f{eqnarray}
+	 * \frac{\partial \hat{P}(g)}{\partial \gamma} & = & -\frac{N \gamma}{g^2} \sqrt{\frac{1-g}{g}} \exp\left[ -\gamma^2 \frac{1-g}{2g} \right]; \\
+	 * \frac{\partial \hat{P}(g)}{\partial N} & = & \frac{1}{\sqrt{g^3(1-g)}} \exp\left[ -\gamma^2 \frac{1-g}{2g} \right].
+	 * \f}
 	 *
 	 * \param[in] fitparam The fitting parameters.
 	 * \param[in] x The independent variables for the function.
