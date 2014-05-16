3f2e1778050b854e95cb84f0ba210b20954cd275
Matt Reuter <mgr522@gmail.com>
2014-05-16 15:21:13 -0500

    Test for fitting to the symmetric, resonant tunneling model.

--- src/tests/fit-symmetric-resonant.py
+++ src/tests/fit-symmetric-resonant.py
@@ -1,1 +1,11 @@
-print "Hello world"
+import subprocess
+
+inputstr = 'SymmetricResonant\nsymmetric-resonant.dat\nnoprint'
+process = subprocess.Popen('../fitter', stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+output = process.communicate(inputstr)
+
+# make sure no errors were reported
+assert(output[1] == '')
+
+# check the output string
+assert(output[0] == 'Resid = 4.310146e-01\ngamma= 9.946e+00, norm= 3.957e+00\n')
