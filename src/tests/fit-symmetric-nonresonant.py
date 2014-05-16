988929c1eb49ad19d0820378085444e62d6c9d71
Matt Reuter <mgr522@gmail.com>
2014-05-16 15:27:46 -0500

    Test for the symmetric, nonresonant model; slight streamline in code for the symmetric, resonant test

--- src/tests/fit-symmetric-nonresonant.py
+++ src/tests/fit-symmetric-nonresonant.py
@@ -1,10 +1,10 @@
 import subprocess
 
 process = subprocess.Popen('../fitter', stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-output = process.communicate('SymmetricResonant\nsymmetric-resonant.dat\nnoprint')
+output = process.communicate('SymmetricNonresonant\nsymmetric-nonresonant.dat\nnoprint')
 
 # make sure no errors were reported
 assert(output[1] == '')
 
 # check the output string
-assert(output[0] == 'Resid = 4.310146e-01\ngamma= 9.946e+00, norm= 3.957e+00\n')
+assert(output[0] == 'Resid = 2.752039e+01\nc= 5.824e+01, d= 9.698e+00, norm= 1.180e+01\n')
