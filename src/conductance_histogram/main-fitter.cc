8a19ca496bb5c17e085928e0544162d3af121e07
Matt Reuter <mgr522@gmail.com>
2014-05-22 12:57:50 -0500

    License for the main conductance histogram programs

--- src/conductance_histogram/main-fitter.cc
+++ src/conductance_histogram/main-fitter.cc
@@ -1,3 +1,6 @@
+/* This file is a part of MolStat, which is distributed under the Creative
+   Commons Attribution-NonCommercial 4.0 International Public License.
+   MolStat (c) 2014, Northwestern University. */
 /**
  * \file main-fitter.cc
  * \brief Main function for fitting histogram data to the desired functional
