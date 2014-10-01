2f41e4db8608c99339c0cd596b0b2bafcd6d54fb
Bo <fubo.nw@gmail.com>
2014-10-01 16:08:56 -0500

    New script.

--- src/electron_transport/simulator_models/runCVODE.sh
+++ src/electron_transport/simulator_models/runCVODE.sh
@@ -2,5 +2,5 @@
 # Script to compile, build and run code using cvode package
 
 g++ -I"/usr/local/include" -c $1 -o $2.o
-g++ -o $2 $2.o "/usr/local/lib/libsundials_cvode.a" "/usr/local/lib/libsundials_nvecserial.a" -lm -lgsl -lgslcblas
+g++ -o $2 $2.o -lsundials_cvode -lsundials_nvecserial -lm -lgsl -lgslcblas
 ./$2
