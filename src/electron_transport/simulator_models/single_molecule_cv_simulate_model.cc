dadd78dc641ba0918ba5f9665d678db0432c7dc3
Bo Fu <fubo.nw@gmail.com>
2014-10-02 22:37:39 -0500

    Bug fixed.

--- src/electron_transport/simulator_models/single_molecule_cv_simulate_model.cc
+++ src/electron_transport/simulator_models/single_molecule_cv_simulate_model.cc
@@ -65,7 +65,7 @@ std::array<double, 2> SingleMoleculeCV::PeakPotentials(shared_ptr<gsl_rng> r)
 	sample(r, params);
 	unpack_parameters(params, e0, eref, lambda, af, ab, v, n, poinitial, temperature, tlimit);
 
-	return {{ 1, peak_potentials(vec) }};
+	return {{1.0 , peak_potentials(params)}};
 }
 
 double SingleMoleculeCV::peak_potentials(const std::vector<double> &vec) {
