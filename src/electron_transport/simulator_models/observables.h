7d882163a4c3f727e3ade19e0e979d406a6131b3
Matt Reuter <mgr522@gmail.com>
2014-12-14 19:46:30 -0500

    Fix a typo in the documentation.

--- src/electron_transport/simulator_models/observables.h
+++ src/electron_transport/simulator_models/observables.h
@@ -86,7 +86,7 @@ public:
 	virtual ~DifferentialConductance() = default;
 
 	/**
-	 * \brief Returns the static conductance for a set of model parameters.
+	 * \brief Returns the differential conductance for a set of model parameters.
 	 *
 	 * \param[in] params A set of model parameters.
 	 * \return The differential conductance for the model parameters.
