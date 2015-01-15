c13a9fb6627372ef320fccff53c3c44449e1db21
Matt Reuter <mgr522@gmail.com>
2015-01-15 10:54:40 -0500

    Skeleton of the code and Makefile for the electrochemistry fitter module.

---
+++ src/echem/fitter_models/echem_fit_module.cc
@@ -0,0 +1,30 @@
+/* This file is a part of MolStat, which is distributed under the Creative
+   Commons Attribution-NonCommercial 4.0 International Public License.
+   MolStat (c) 2014, Northwestern University. */
+/**
+ * \file echem_fit_module.cc
+ * \brief Function that loads electrochemistry fit models.
+ *
+ * \author Matthew G.\ Reuter
+ * \date January 2015
+ */
+
+#include "echem_fit_module.h"
+
+using namespace std;
+
+namespace molstat {
+namespace echem {
+
+void load_models(
+	std::map<std::string, FitModelFactory<1>> &models)
+{
+#if 0
+// preserve the code for ease of adding a model, once one's ready
+	models["symmetricresonant"] =
+		GetFitModelFactory<SymmetricResonantFitModel, 1>();
+#endif
+}
+
+} // namespace molstat::echem
+} // namespace molstat
---
+++ src/echem/fitter_models/echem_fit_module.h
@@ -0,0 +1,37 @@
+/* This file is a part of MolStat, which is distributed under the Creative
+   Commons Attribution-NonCommercial 4.0 International Public License.
+   MolStat (c) 2014, Northwestern University. */
+/**
+ * \file echem_fit_module.h
+ * \brief Loads electrochemistry models for fitting experimental data.
+ *
+ * \author Matthew G.\ Reuter
+ * \date January 2015
+ */
+
+#ifndef __echem_fit_models_h__
+#define __echem_fit_models_h__
+
+#include <general/fitter_tools/fit_model_interface.h>
+#include <map>
+#include <string>
+
+namespace molstat {
+namespace echem {
+
+/**
+ * \brief Loads the electrochemistry models into the MolStat "database".
+ *
+ * Models are stored as a map from a string (the name of the model) to a
+ * factory for the model (of type FitModelFactory).
+ *
+ * \param[in,out] models The map of models in MolStat. On output, the models
+ *    for transport have been added to it.
+ */
+void load_models(
+	std::map<std::string, FitModelFactory<1>> &models);
+
+} // namespace molstat::echem
+} // namespace molstat
+
+#endif
