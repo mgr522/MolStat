3aec1946683d608573ab53d783a07c1fa0ae94c3
benwu1 <benwu@email.arizona.edu>
2016-01-11 15:34:59 -0700

    Update observables.h

--- src/electron_transport/simulator_models/observables.h
+++ src/electron_transport/simulator_models/observables.h
@@ -122,6 +122,25 @@ public:
 	virtual double DiffG(const std::valarray<double> &params) const = 0;
 };
 
+/// Observable class for distance
+class Distance : public Observable<Distance>
+{
+public:
+	Distance()
+		: Observable<Distance>(&Distance::DistD)
+	{}
+
+	virtual ~Distance() = default;
+
+	/**
+	 * \brief Returns the distance for a set of model parameters.
+	 *
+	 * \param[in] params A set of model parameters.
+	 * \return The distance for the model parameters.
+	 */
+	virtual double DistD(const std::valarray<double> &params) const = 0;
+};
+
 } // namespace molstat::transport
 } // namespace molstat
 
